9b8505a86df59da05f41f2f0b3b43fb4d2285845
Kequc <mr.kequc@gmail.com>
2017-02-18 01:15:06 +0100

    Tests for functions

--- test/json-extend.spec.js
+++ test/json-extend.spec.js
@@ -31,7 +31,7 @@ describe('jsonExtend', function () {
             expect(result).to.eql(source);
         });
         it('removes undefined values', function () {
-            const target = { existing: 'data' }
+            const target = { existing: 'data' };
             const source = { hello: 0, cat: 'dog', existing: undefined };
             const result = jsonExtend(target, source);
             expect(result).to.equal(target);
@@ -40,21 +40,35 @@ describe('jsonExtend', function () {
     });
     describe('objects', function () {
         it('merges the source with the target', function () {
-            const target = { existing: 'data' }
+            const target = { existing: 'data' };
             const source = { hello: 0, cat: 'dog', existing: 'overwritten' };
             const result = jsonExtend(target, source);
             expect(result).to.equal(target);
             expect(result).to.eql({ existing: 'overwritten', hello: 0, cat: 'dog' });
         });
         it('deep merges the source with the target', function () {
-            const target = { existing: 'data', deep: { hey: 'there!', num: 21 } }
+            const target = { existing: 'data', deep: { hey: 'there!', num: 21 } };
             const source = { hello: 0, cat: 'dog', deep: { hey: 'overwritten' } };
             const result = jsonExtend(target, source);
             expect(result).to.equal(target);
             expect(result).to.eql({ existing: 'data', hello: 0, cat: 'dog', deep: { hey: 'overwritten', num: 21 } });
         });
+        it('overwrites arrays with objects', function () {
+            const target = { existing: ['data'] };
+            const source = { existing: { hmm: 'overwritten' } };
+            const result = jsonExtend(target, source);
+            expect(result).to.equal(target);
+            expect(result).to.eql(source);
+        });
     });
     describe('arrays', function () {
+        it('overwrites a target non-array', function () {
+            const target = { array: 200 };
+            const source = { array: ['new one', 11] };
+            const result = jsonExtend(target, source);
+            expect(result).to.equal(target);
+            expect(result).to.eql(source);
+        });
         it('overwrits a target array with source', function () {
             const target = { array: ['hello', 'there'] };
             const source = { array: ['new'] };
@@ -69,14 +83,34 @@ describe('jsonExtend', function () {
             expect(result).to.equal(target);
             expect(result).to.eql(source);
         });
-        it('overwrites a target non-array', function () {
-            const target = { array: 200 };
-            const source = { array: ['new one', 11] };
-            const result = jsonExtend(target, source);
-            expect(result).to.equal(target);
-            expect(result).to.eql(source);
-        });
     });
     describe('functions', function () {
+        it('delivers the old value', function () {
+            const old = 'hi';
+            const func = (value) => {
+                expect(value).to.equal(old);
+            };
+            jsonExtend({ old }, { old: func });
+        });
+        it('delivers arrays', function () {
+            const old = { what: 'up' };
+            const func = (value) => {
+                expect(value).to.equal(old);
+            };
+            jsonExtend({ old }, { old: func });
+        });
+        it('delivers objects', function () {
+            const old = ['array content', 111];
+            const func = (value) => {
+                expect(value).to.equal(old);
+            };
+            jsonExtend({ old }, { old: func });
+        });
+        it('runs inside arrays with undefined', function () {
+            const func = (value) => {
+                expect(value).to.equal(undefined);
+            };
+            jsonExtend({ old: ['no'] }, { old: [func] });
+        });
     });
 });
